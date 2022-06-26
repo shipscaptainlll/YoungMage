6ee560e355858d8c05deb9e2424eec404a897e9e
shipscaptainlll <shipscaptainlll@gmail.com>
2022-06-26 23:32:22 +0200

    0.0.59
    -Added fire for torches VFX + scene
    -Added fire for chandalers + scene
    -Added Movement dust from mage and skeleton + scene
    -Added Jumping and landing dust from mage + scene
    -Added glow and particles for legendary pickaxe in scene
    -Object creation blast for alchemist table + scene
    -Added midas cauldron electricity vfx + scene
    -Sceletons portal VFX placed in Scene

--- Assets/Scripts/MainCharacter/PersonMovement.cs
+++ Assets/Scripts/MainCharacter/PersonMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.VFX;
 
 public class PersonMovement : MonoBehaviour
 {
@@ -17,14 +18,26 @@ public class PersonMovement : MonoBehaviour
     [SerializeField] float gravity;
     float checkRadius;
     bool occupied;
+    VisualEffect movementVFX;
+    VisualEffect jumpVFX;
+    VisualEffect landVFX;
+
+    float xInput;
+    float zInput;
 
     bool isGrounded;
+
     void Start()
     {
+        
         occupied = false;
         checkRadius = 0.5f;
+        movementVFX = transform.Find("VFX").Find("vfxgraph_StylizedSmoke").GetComponent<VisualEffect>();
+        jumpVFX = transform.Find("VFX").Find("vfxgraph_StylizedSmokeJump").GetComponent<VisualEffect>();
+        landVFX = transform.Find("VFX").Find("vfxgraph_StylizedSmokeLand").GetComponent<VisualEffect>();
         _characterOccupation.CharacterEngagedSomething += PreventMoving;
         _characterOccupation.CharacterDisengagedSomething += EnableMoving;
+        StartCoroutine(MovingDustSpawner());
     }
 
     void Update()
@@ -33,15 +46,22 @@ public class PersonMovement : MonoBehaviour
         MoveCharacter();
     }
 
+    
+
     void MoveCharacter()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        bool isGroundedOld = isGrounded;
+        xInput = Input.GetAxis("Horizontal");
+        zInput = Input.GetAxis("Vertical");
         
-        Vector3 move = transform.right * x + transform.forward * z;
+        Vector3 move = transform.right * xInput + transform.forward * zInput;
+        
         characterController.Move(move * speed * Time.deltaTime);
 
         isGrounded = (Physics.CheckSphere(checkGround.position, checkRadius, checkLayer) || Physics.CheckSphere(checkGround.position, checkRadius, checkLayer2));
+        if (isGrounded && !isGroundedOld) {
+            landVFX.SendEvent("CharacterLanded"); landVFX.SetVector3("SphereCenterPosition", landVFX.transform.position);
+        }
 
         if (isGrounded && velocity.y < 0)
         {
@@ -50,11 +70,23 @@ public class PersonMovement : MonoBehaviour
 
         if (isGrounded && Input.GetButtonDown("Jump"))
         {
+            jumpVFX.SetVector3("SphereCenterPosition", jumpVFX.transform.position); 
+            jumpVFX.SendEvent("CharacterJumped");
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator MovingDustSpawner()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.2f);
+            if (isGrounded && (Mathf.Abs(xInput) + Math.Abs(zInput)) > 0) { movementVFX.SendEvent("CharacterMoved"); }
+        }
     }
 
     void PreventMoving()
